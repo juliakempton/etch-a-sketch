2d0b2d773b993b4bd083e856f5a5c9e5332e3f4a
Julia Kempton <kemptonjq@g.cofc.com>
2021-07-01 19:30:47 -0400

    Created an effect that makes grid divs turn black when the mouse passes over them

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const gridContainer = document.querySelector("#grid-squares-container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     gridContainer.appendChild(div);
     div.classList.add("grid-box");
 }
 
 
+const gridBoxes = document.querySelectorAll(".grid-box");
+gridBoxes.forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+        console.log("enter");
+        box.style.backgroundColor = "black";
+    })
+})
