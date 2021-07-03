1980664e6e3a94bab32be7f8a362f5ec87d8b4e6
Julia Kempton <kemptonjq@g.cofc.com>
2021-07-03 12:19:36 -0400

    Added a button that allows user to clear the grid and create a new grid with a user-determined number of squares per side between 0 and 100

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const gridContainer = document.querySelector("#grid-squares-container");
+const clearBtn = document.querySelector("#clear")
 
-for (let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-    gridContainer.appendChild(div);
-    div.classList.add("grid-box");
+
+
+function createGrid(numSquares) {
+    let totalNumSquares = numSquares * numSquares;
+    for (let i = 0; i < totalNumSquares; i++) {
+        let div = document.createElement("div");
+        gridContainer.appendChild(div);
+        div.classList.add("grid-box");
+        div.style.height = `${100 / numSquares}%`;
+        div.style.width = `${100 / numSquares}%`;
+    }
+    let gridBoxes = document.querySelectorAll(".grid-box");
+    gridBoxes.forEach((box) => { 
+        box.addEventListener("mouseover", (e) => {
+            box.style.backgroundColor = "black";
+        })
+    }) 
+}
+createGrid(16);
+
+
+function numSquaresPrompt() {
+    let numSquares;
+    while (isNaN(numSquares) || numSquares < 0 || numSquares > 100) {
+    numSquares = window.prompt(`How many squares per side should the new grid have? \nEnter a number between 0 and 100`);
+        }
+    createGrid(numSquares);
 }
 
 
-const gridBoxes = document.querySelectorAll(".grid-box");
-gridBoxes.forEach((box) => {
-    box.addEventListener("mouseenter", () => {
-        console.log("enter");
-        box.style.backgroundColor = "black";
-    })
-})
+
+
+function resetBoard() {
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+clearBtn.addEventListener("click", resetBoard);
+
+clearBtn.addEventListener("click", numSquaresPrompt);
